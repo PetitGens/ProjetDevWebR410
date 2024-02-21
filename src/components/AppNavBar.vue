2e0a411f543458dc51e693370f8370146e7dc4f8
Julien Ait azzouzene <julien.azzo27@gmail.com>
2024-02-21 15:41:55 +0100

    ✨ feat: logout

--- src/components/AppNavBar.vue
+++ src/components/AppNavBar.vue
@@ -1,13 +1,29 @@
 <script setup>
 import { useUserStore } from '@/stores/user';
+import { supabase } from '@/supabase';
 import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const { user } = storeToRefs(useUserStore());
+
+const router = useRouter();
+
+const loading = ref(false);
+
+async function logout() {
+    loading.value = true;
+    await supabase.auth.signOut();
+    router.push({ name: 'login' }); 
+}
 </script>
 
 <template>
     <header class="flex items-align p-2">
         <div v-if="user">Username : {{ user.username }}</div>
-        <button class="ml-auto rounded-md outline p-2">Se déconnecter</button>
+        <button :loading="loading" @click="logout" class="ml-auto rounded-md outline p-2">
+            <span v-if="loading">...</span>
+            <span v-else>Se déconnecter</span>
+        </button>
     </header>
 </template>
